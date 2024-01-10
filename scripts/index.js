0442846fb01a0958e81de2114112b0f52c421fdb
Boris Butorin <butorinb.g.personal@gmail.com>
2024-01-10 23:40:07 +0700

    script_added

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,48 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+//Создаем объект из элемента HTML
+
+const cardList = document.querySelector('.places__list');
+
+//Задаем функцию создания карточки и передаем ей функцию удаления карточки в качестве колбэка
+
+function createCard(item){
+    const cardTemplate = document.querySelector('#card-template').content;
+    const newCardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    
+    newCardElement.querySelector('.card__image').src = item.link;
+    newCardElement.querySelector('.card__image').alt = item.name;
+    newCardElement.querySelector('.card__title').textContent = item.name;
+
+    const deleteButton = newCardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click',() => deleteCard(newCardElement));
+
+    return newCardElement;
+}
+
+
+//Создаем функцию добавления карточек на страницу
+
+initialCards.forEach((item) => {
+    const card = createCard(item, deleteCard);
+    cardList.append(card);
+})
+
+
+//Создаем функцию удаления карточек со страницы
+
+function deleteCard(card){
+    card.remove();
+}
+
+
+
+
+
+
+
+
+ 
